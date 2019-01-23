df700da0e03b8fd5c0ef7deefd4688fef3949565
flavienfr <44776722+flavienfr@users.noreply.github.com>
2019-01-23 16:40:25 +0100

    Version adapter

--- LoRaSender.ino
+++ LoRaSender.ino
@@ -1,32 +1,28 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-int debit = 0;
-int tmp = 25.3;
+float debit = 0;
+float tmp = 25.3;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  Serial.println("LoRa Sender");
-
   if (!LoRa.begin(915E6)) {
-    Serial.println("Starting LoRa failed!");
+    Serial.println("666;666");
     while (1);
   }
 }
 
 void loop() {
-
   // send packet
   LoRa.beginPacket();
-  LoRa.print("D:");
   LoRa.print(debit);
-  LoRa.print("T:");
+  LoRa.print(";");
   LoRa.print(tmp);
   LoRa.endPacket();
 
   debit++;
 
-  delay(50);
+  delay(1000);
 }
