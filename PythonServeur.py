9e7bd46c58052e46a13253aa68ab65ef02180c28
flavienfr <44776722+flavienfr@users.noreply.github.com>
2019-03-12 11:07:49 +0100

    Add files via upload

--- PythonServeur.py
+++ PythonServeur.py
@@ -5,13 +5,14 @@ ser = serial.Serial('/dev/serial0', 9600)
 
 while True:
 	data = ser.readline()
-	if data.find(";") > 0:
+	if data[0]==";":
 		#print(data)
 		data = data.split(";")
-		tmp = data[0]
-		debit = data[1]
+		add = data[1]
+		tmp = data[2]
+		debit = data[3]
 		print "Save in DB"
+		print "addresse :",add
 		print "temperature :",tmp
 		print "Debit :",debit
-	
 
