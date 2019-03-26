556d503ea0acb3485ac912a9e5699d32ff3efb17
flavienfr <44776722+flavienfr@users.noreply.github.com>
2019-03-26 11:50:15 +0100

    Add files via upload

--- PythonServeur.py
+++ PythonServeur.py
@@ -1,31 +1,21 @@
 import serial
 import string
-#import mysql.connector 
 
 ser = serial.Serial('/dev/serial0', 9600)
 
 while True:
 	data = ser.readline()
 	if data[0]==";":
-		#print(data)
+		print(data)
 		data = data.split(";")
 		add = data[1]
 		tmp = data[2]
 		debit = data[3]
+		ser.write([123])
+		
 		print "Save in DB"
 		print "addresse :",add
 		print "temperature :",tmp
 		print "Debit :",debit
-
-	#conn = mysql.connector.connect(host="127.0.0.1",user="root",password="raspberry", 		database="dressthing")
-	#cursor = conn.cursor()
-
-	#requete = "INSERT INTO `dressthing`.`Capteurs` (`magnetique`, `couleur`) VALUES (%s, %s)"
-	#valeurs = (magnetique,couleur)
-	
-	#cursor.execute(requete,valeurs)
-
-	#conn.commit()
-
-	#conn.close()
+		
 
