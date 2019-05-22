1d4e189ec67d18ddd580867023d1439058564941
flavienfr <44776722+flavienfr@users.noreply.github.com>
2019-05-22 11:54:38 +0200

    Add files via upload

--- communication_vers_visualisation.py
+++ communication_vers_visualisation.py
@@ -6,29 +6,32 @@ ser = serial.Serial('/dev/serial0', 9600)
 
 while True:
 	data = ser.readline()
-	if data[0]==";": #ajouter si code fonction == 1 alors ...
+	if data[0]==";": 
 		print(data)
 		data = data.split(";")
-		add = data[1]
-		tmp = data[2]
-		debit = data[3]
-		ser.write([123])
-		#supprimer les print
-		print "Save in DB"
-		print "addresse :",add
-		print "temperature :",tmp
-		print "Debit :",debit
+		if data[1] == "1": #0rien;1fonction;2add;3tmp;4debit
+			fonction = data[1]
+			add = data[2]
+			tmp = data[3]
+			debit = data[4]
+			ser.write([123])
+			#supprimer les print
+			print "Save in DB"
+			print "fonction :",fonction
+			print "addresse :",add
+			print "temperature :",tmp
+			print "Debit :",debit
 
-		conn = mysql.connector.connect(host="mysql-ormeaux.alwaysdata.net",user="ormeaux",password="G27&$Ar2", database="ormeaux_29")
-		cursor = conn.cursor()
-		cursor = conn.cursor()
+			conn = mysql.connector.connect(host="mysql-ormeaux.alwaysdata.net",user="ormeaux",password="G27&$Ar2", database="ormeaux_29")
+			cursor = conn.cursor()
+			cursor = conn.cursor()
 
-		requete = "INSERT INTO mesures(id_bassins,temperature, debit) VALUES (%s, %s, %s)"
-		valeurs = (add,tmp,debit)
+			requete = "INSERT INTO mesures(id_bassins,temperature, debit) VALUES (%s, %s, %s)"
+			valeurs = (add,tmp,debit)
 	
-		cursor.execute(requete,valeurs)
+			cursor.execute(requete,valeurs)
 
-		conn.commit()
+			conn.commit()
 
-		conn.close()
+			conn.close()
 		
