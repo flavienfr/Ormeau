4293067ac59cab3ed8b5f947e5ffb39b47e16420
flavienfr <44776722+flavienfr@users.noreply.github.com>
2019-05-22 11:07:31 +0200

    Add files via upload

--- testdatabase.py
+++ testdatabase.py
@@ -1,18 +1,18 @@
 
 import mysql.connector 
 
+idx = 2
+tmp = 53.3
+db = 0
 
-tmp = 25.0
-db = 1.3
 
-
-#conn = mysql.connector.connect(host="ormeaux-29.mysql.database.azure.com",user="ormeaux@ormeaux-29",password="Password29", database="ormeaux")
-conn = mysql.connector.connect(host="10.160.108.85",user="ormeaux",password="password", database="ormeaux")
+conn = mysql.connector.connect(host="mysql-ormeaux.alwaysdata.net",user="ormeaux",password="G27&$Ar2", database="ormeaux_29")
+#conn = mysql.connector.connect(host="10.160.108.85",user="ormeaux",password="password", database="ormeaux")
 cursor = conn.cursor()
 cursor = conn.cursor()
 
-requete = "INSERT INTO `ormeaux`.`mesures` (`temppinerature`, `debit`) VALUES (%s, %s)"
-valeurs = (tmp,db)
+requete = "INSERT INTO mesures(id_bassins,temperature, debit) VALUES (%s, %s, %s)"
+valeurs = (idx,tmp,db)
 	
 cursor.execute(requete,valeurs)
 
